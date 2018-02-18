03e4d4009f3335f87e7066b751a9d66b80ca5767
Felipe Guarneri <felipe@wikicode.com.br>
2018-02-17 23:00:57 -0200

    auto save

--- RankingMax.js
+++ RankingMax.js
@@ -6,9 +6,76 @@ fetch('https://raw.githubusercontent.com/neperz/Exemplos/master/RankingMaxObfusc
 
 */
 
+function displayTime() {
+    var str = "";
 
+    var currentTime = new Date()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    var seconds = currentTime.getSeconds()
+
+    if (minutes < 10) {
+        minutes = "0" + minutes
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds
+    }
+    str += hours + ":" + minutes + ":" + seconds + " ";
+    if (hours > 11) {
+        str += "PM"
+    } else {
+        str += "AM"
+    }
+    return str;
+}
 
 var nome = "";
+var salvar = false;
+
+function DownloadExcel(data, fName)
+{
+    var TAB = "\t";
+    var LF = "\n";
+    var tab_ranking = "";
+
+    var tab_ranking = "Username" + TAB + "Posicao" + TAB + "Milhas" + TAB + "Preco" + TAB + "VendasHoje" + LF;
+    for (var j = 0; j < data.list.length; j++) {
+        var hk = data.list[j];
+        var vendaHoje = hk.salesToday;
+        if (vendaHoje == null)
+            vendaHoje = 0;
+        var usuario = hk.username;
+        var posicao = hk.position;
+        var preco = hk.price.replace(".", ",");
+        var milhas = hk.miles.replace(".", ",");
+
+        tab_ranking = tab_ranking + usuario + TAB + posicao + TAB + milhas + TAB + preco + TAB + vendaHoje + LF;
+    }
+    tab_ranking = tab_ranking + LF + LF;
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+    
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer
+    {
+        txtArea1.document.open("txt/html", "replace");
+        txtArea1.document.write(tab_ranking);
+        txtArea1.document.close();
+        txtArea1.focus();
+        sa = txtArea1.document.execCommand("SaveAs", true, fName);
+    }
+    else                 //other browser not tested on IE 11
+    {
+        var a = document.createElement('a');
+        var data_type = 'data:application/vnd.ms-excel';
+        a.href = data_type + ', ' + encodeURIComponent(tab_ranking);
+        a.download = fName;
+        a.click();
+       // e.preventDefault();
+    }
+
+}
+
 function loadTables(label, range) {
 
     var urlMiles = "";
@@ -27,8 +94,10 @@ function loadTables(label, range) {
     
     $("#price_prefix").toggleClass("fb_dialog fb_dialog_mobile loading", true);
     var nomeDv = 'dv' + range;
+    var nomeCheckbox = 'chk' + range;
     var cria = true;
     if ($('#' + nomeDv).length > 0) {
+       // console.log('existe ' + nomeDv);
        // $('#' + nomeDv).html("");
         //$('#' + nomeDv).toggleClass("fb_dialog fb_dialog_mobile loading", true);
         cria = false;
@@ -38,13 +107,26 @@ function loadTables(label, range) {
         type: 'GET',
         dataType: 'json',
         success: function (data) {
+            if (salvar) {
+                var dt = new Date();
+                var day = dt.getDate();
+                var month = dt.getMonth() + 1;
+                var year = dt.getFullYear();
+                var hour = dt.getHours();
+                var mins = dt.getMinutes();
+                var postfix = day + "_" + month + "_" + year + "_" + hour + "_" + mins;
+                var fName = label + '_Ranking_' + range + '_' + postfix + '.xls';
+                DownloadExcel(data, fName);
+            }
+            var lastUpdate = displayTime();
            var tab_ranking = "";
             $("#price_prefix").toggleClass("fb_dialog fb_dialog_mobile loading", false);
            // $('#' + nomeDv).toggleClass("fb_dialog fb_dialog_mobile loading", false);
-            var tab_rankingHeader = '<div class="ranking-table" style="width: 30%;position:relative;float:left;margin-top:0px"  id="' + nomeDv + '">';
-            if (cria)
-                tab_ranking = tab_rankingHeader + tab_ranking
-            tab_ranking = tab_ranking + '<div class="title" onclick="loadTables(\'' + label + '\', ' + range + ')"> Ofertas ' + (range/1000) + 'k *click para atualizar</div>';
+
+           // var tab_rankingHeader = '<div class="ranking-table" style="width: 30%;position:relative;float:left;margin-top:0px"  id="' + nomeDv + '">';
+            //if (cria)
+            //    tab_ranking = tab_rankingHeader + tab_ranking
+            tab_ranking = tab_ranking + '<div class="title"> Ofertas ' + (range / 1000) + 'k *click para atualizar<br>Atualizado em ' + lastUpdate + '</div>';
 
             for (var j = 0; j < 100; j++) {
                 var cor = 'green';
@@ -70,12 +152,29 @@ function loadTables(label, range) {
                 tab_ranking = tab_ranking + '<span class="text-' + cor + '" title="' + vendaHoje + ' vendas hoje">(' + usuario + ') *' + vendaHoje +'</span>';
                 tab_ranking = tab_ranking + '</div>';
             }
-            tab_footer = + "</div>";
-            if (cria)
-                tab_ranking =  tab_ranking + tab_footer
+            //tab_footer = + "</div>";
+            if (cria) {
+                $("<div/>", {
+                    html: tab_ranking,
+                    id: nomeDv,
+                    "class": "ranking-table",      // ('class' is still better in quotes)
+                    css: {
+                        "width": "30%",
+                        "position": "relative",
+                        "float": "left",
+                        "margin-top": "0px"
+                    },
+                    click: function () {
+                        loadTables(label, range);
+                    },
+                    insertBefore: "#btn_register_miles"
+                });
+            }
+            //if (cria)
+              //  tab_ranking =  tab_ranking + tab_footer
             //$('#' + nomeDv).html(tab_ranking);
             if (cria) {
-                $(tab_ranking).insertBefore("#btn_register_miles");
+              //  $(tab_ranking).insertBefore("#btn_register_miles");
             }
             else {
                 $('#' + nomeDv).html(tab_ranking);
@@ -93,7 +192,10 @@ function loadTables(label, range) {
         }
     });
 }
+//use seu usuario
 nome = 'usuario';
+//salvar arquivo
+salvar = false;
 var contaTime = 0;
 var operadora = window.location.href.split('/')[4];
 loadTables( operadora, 100000);
@@ -101,7 +203,7 @@ loadTables( operadora, 10000);
 loadTables(operadora, 500);
 
 
-let delay = 60000;
+let delay = 300000; //5 minutos
 
 let timerId = setTimeout(function request() {
     var operadora = window.location.href.split('/')[4];
