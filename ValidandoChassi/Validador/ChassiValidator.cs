308f91504591f6729a74e4515d210628dec2b874
Felipe Mendonça <felipe@wikicode.com.br>
2021-04-25 18:45:55 -0300

    chore(testando): tentando validar

---
+++ ValidandoChassi/Validador/ChassiValidator.cs
@@ -0,0 +1,95 @@
+using System;
+namespace Validador
+{
+    public class ChassiValidator
+    {
+        private string _chassi { get; set; }
+        public ChassiValidator(string chassi)
+        {
+            _chassi=chassi;
+        }
+        public bool Validar()
+        {
+            //KMHJU819BCU368279
+            int soma, numero, resto;
+            string digito;
+            if (_chassi.Length!=17)
+                throw new System.Exception("Numero do chassi deve ter 17 caracteres");
+            soma=0;
+            for (int i = 0; i < _chassi.Length-1; i++)
+            {
+                var cPart = _chassi[i];
+                if (char.IsNumber(cPart))          
+                {      
+                    numero= int.Parse(cPart.ToString());
+                    Console.WriteLine($"[{i}]: {numero.ToString()} é número");
+                }
+                else
+                {
+                    numero= GetValByLetter(cPart);
+                    Console.WriteLine($"[{i}]: {cPart} NÃO é número");
+                }
+                switch (numero)
+                {
+                    case 1: case 11: soma += numero *8;break;
+                    case 2:case 12:  soma +=   numero * 7;break;
+                    case 3 :case 13:  soma +=   numero * 6;break;
+                    case  4:case  14:  soma +=  numero * 5;break;
+                    case 5: case 15:  soma +=   numero * 4;break;
+                    case  6: case 16:  soma +=   numero * 3;break;
+                    case 7:case 17:  soma +=   numero * 2;break;
+                    case 8   :  soma +=   numero * 10;break;
+                    case 9   :  soma +=   numero * 0;break;
+                    case 10  :  soma +=   numero * 9;      break;              
+                    default:
+                        break;
+                }
+                Console.WriteLine($"Soma: {soma}");
+            
+            }
+            resto = (soma *10 ) % 11;
+            Console.WriteLine($"Resto: {resto}");
+            if (resto==10)
+                digito="X";
+            else            
+                digito = resto.ToString();
+            Console.WriteLine($"Digito: {digito}");    
+            return digito== _chassi[8].ToString();
+        }
+        private int GetValByLetter(char letra)
+        {
+            int letraInt=0;
+            switch (letra)
+            {
+                case 'A': letraInt=1;break;
+                case 'B': letraInt=2;break;
+                case 'C': letraInt=3;break;
+                case 'D': letraInt=4;break;
+                case 'E': letraInt=5;break;
+                case 'F': letraInt=6;break;
+                case 'G': letraInt=7;break;
+                case 'H': letraInt=8;break;
+                case 'I': letraInt=9;break;
+                case 'J': letraInt=1;break;
+                case 'K': letraInt=2;break;
+                case 'L': letraInt=3;break;
+                case 'M': letraInt=4;break;
+                case 'N': letraInt=5;break;
+                case 'O': letraInt=6;break;
+                case 'P': letraInt=7;break;
+                case 'Q': letraInt=8;break;
+                case 'R': letraInt=9;break;
+                case 'S': letraInt=2;break;
+                case 'T': letraInt=3;break;
+                case 'U': letraInt=4;break;
+                case 'V': letraInt=5;break;
+                case 'W': letraInt=6;break;
+                case 'X': letraInt=7;break;
+                case 'Y': letraInt=8;break;                 
+                default:
+                letraInt=9;break;   
+            }
+            return letraInt;
+        }
+    }
+}
---
+++ ValidandoChassi/Validador/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Validador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string chassiToValidate = args[0];
+            Console.WriteLine($"validando chassi {chassiToValidate}");
+            ChassiValidator cv = new ChassiValidator(chassiToValidate);
+            var result = cv.Validar();
+            if (result)
+            Console.WriteLine("Chassi OK");
+            else
+            Console.WriteLine("Chassi Invalido");
+        }
+    }
+}
